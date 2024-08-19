45483080ce2f6ed0c49b73e1540d4ac0f56356a8
LegacyGuy <prathameshkashid26@gmail.com>
2024-08-19 17:22:47 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -13,6 +13,7 @@ public class Tshirts {
          assert(size(37) == "S");
         assert(size(38) == "S");
         assert(size(40) == "M");
+        assert(size(42) == "M");
         assert(size(43) == "L");
         assert(size(50) == "L");
         System.out.println("All is well (maybe!)");
